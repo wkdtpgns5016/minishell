f586809894399f29ef3a730f52ed974f3536f617
sunwchoi <101624921+sunwchoi@users.noreply.github.com>
2022-07-20 14:17:32 +0900

    signal process (#16)
    1. main -> readline
    2. ctrl + c -> signal control
    3. ctrl + d -> terminal control
    4. ctrl + \ -> ignored
    
    resolve #5
    resolve #12
    resolve #13
    resolve #14
    
    Co-authored-by: 최선우 <sunwchoi@choeseon-uui-MacBookAir.local>

---
+++ includes/minishell.h
@@ -0,0 +1,13 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <signal.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <termios.h>
+#include "./signal.h"
+
+#endif
