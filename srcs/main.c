c60bf620444c4676914552813deb91605fb20bf8
sehjang <74850752+wkdtpgns5016@users.noreply.github.com>
2022-07-20 17:33:21 +0900

    Feat: readline 데이터 처리 및 데이터 파싱 (#22)
    1. 명령어 구조체 구현
    2. readline으로 받은 문자열을 데이터 처리 후 구조체로 구성
    3. 메모리 누수 방지
    
    resolved #20

--- srcs/main.c
+++ srcs/main.c
@@ -1,9 +1,12 @@
 #include "../includes/minishell.h"
 
-int main()
+int	main(int ac, char **av, char **envp)
 {
 	char	*line;
+	t_info	info;
 
+	if (ac < 1 || av == 0 || envp == 0)
+		exit(1);
 	set_terminal();
 	signal(CTRL_C, handler);
 	while (1)
@@ -12,10 +15,18 @@ int main()
 		if (!line)
 		{
 			printf("\033[1A");
-        	printf("\033[11C");
+			printf("\033[11C");
 			printf("exit\n");
 			return (0);
 		}
+		/* data parsing */
+		info = set_info(line, envp);
+
+		/* command processing */
+
+		/* free memmory */
+		free_cmds(info.cmds);
+		ft_free((void **)&line);
 	}
 	ret_terminal();
 }
