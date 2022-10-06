cfe15b5c459afcfd68b620e1262c1b8318577fe8
sehjang <74850752+wkdtpgns5016@users.noreply.github.com>
2022-10-06 18:26:29 +0900

    Fix: 메모리 누수 해결 (#172)
    1. heredoc 메모리 누수 해결
    
    resovled #171

--- srcs/parsing/parsing_utils5.c
+++ srcs/parsing/parsing_utils5.c
@@ -20,43 +20,51 @@ int	change_values_arr(char ***cmd, char *str1, char *str2, int i)
 	ft_free((void **)&(*cmd)[i + 1]);
 	(*cmd)[i] = ft_strdup(str1);
 	if ((*cmd)[i] == 0)
-	{
-		ft_free_arr((char ***)cmd);
 		return (0);
-	}
 	(*cmd)[i + 1] = ft_strdup(str2);
 	if ((*cmd)[i + 1] == 0)
-	{
-		ft_free_arr((char ***)cmd);
 		return (0);
-	}
 	return (1);
+}
+
+int	sub_make_heredoc(t_cmds *cmds, t_info *info, int index, char ***cmd)
+{
+	if (g_signal_flag == 2)
+		return (1);
+	if (ft_strncmp((*cmd)[index], "<<", 2) == 0)
+	{
+		if (cmds->heredoc_filepath != 0)
+			ft_free((void **)&cmds->heredoc_filepath);
+		cmds->heredoc_filepath = get_heredoc((*cmd)[index + 1], info);
+		cmds->heredoc_flag = 1;
+		if (cmds->heredoc_filepath == 0)
+			return (-1);
+		if (change_values_arr(cmd, "<", cmds->heredoc_filepath, index) == 0)
+			return (-1);
+	}
+	return (0);
 }
 
 char	**make_heredoc(char *content, t_cmds *cmds, t_info *info)
 {
 	char	**cmd;
 	int		i;
+	int		flag;
 
 	i = -1;
 	cmd = ft_split(content, ' ');
+	flag = 1;
 	if (cmd == 0)
 		return (0);
 	while (cmd[++i] != 0)
 	{
-		if (g_signal_flag == 2)
+		flag = sub_make_heredoc(cmds, info, i, &cmd);
+		if (flag == 1)
 			break ;
-		if (ft_strncmp(cmd[i], "<<", 2) == 0)
+		else if (flag == -1)
 		{
-			cmds->heredoc_filepath = get_heredoc(cmd[i + 1], info);
-			cmds->heredoc_flag = 1;
-			if (cmds->heredoc_filepath == 0)
-			{
-				ft_free_arr((char ***)&cmd);
-				return (0);
-			}
-			if (change_values_arr(&cmd, "<", cmds->heredoc_filepath, i) == 0)
-				return (0);
+			ft_free_arr((char ***)&cmd);
+			return (0);
 		}
 	}
 	return (cmd);
