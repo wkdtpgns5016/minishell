1c7d0fbb154a15b23f48264c2f2f3394d61c9bab
sehjang <74850752+wkdtpgns5016@users.noreply.github.com>
2022-10-10 18:17:01 +0900

    Feat: 파이프 요소가 붙은 토큰 분할 (#192)
    1. 파이프 요소가 붙은 토큰도 분할 하도록 기능 추가
    
    resolved #191

--- includes/tokenizing.h
+++ includes/tokenizing.h
@@ -5,8 +5,8 @@
 
 char 	**divide_line(char *line);
 t_cmds	*insert_cmds(char **token, t_info *info);
-char	**divide_redir_garbage(char **token);
-char	**create_divide_redir(char *redir);
+char	**divide_token_garbage(char **token);
+char	**create_divide_token(char *redir);
 t_cmds	*new_set_cmds(t_info *info, char *line);
 void	interpret_cmds(t_cmds *cmds, t_info *info);
 void	interpret_dollar(t_list **quote_lst, t_info *info);
